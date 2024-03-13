34c29e2175b6c5a704e26d6e01ae3db1610f2b3f
04stefke <jovanovics399@yahoo.com>
2024-03-13 11:22:04 +0000

    added the functionality for second step

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const multiStepForm = document.querySelector('[data-multi-step-form]')
 const formSteps = [...multiStepForm.querySelectorAll('[data-step]')]
+const stepCount = [...document.querySelectorAll('[data-step-number]')]
+
+const formPlans = [...multiStepForm.querySelectorAll('[data-plan]')]
 
 let currentStep = formSteps.findIndex(step => {
     return step.classList.contains('active')
 })
-console.log(currentStep)
+
+let currentStepCount = stepCount.findIndex(count => {
+    return count.classList.contains('active')
+})
+
 if(currentStep < 0){
     currentStep = 0
     showCurrentStep()
 }
-console.log(currentStep)
+
+if(currentStepCount < 0) {
+    currentStepCount = 0
+    showCurrentCountStep()
+}
 
 multiStepForm.addEventListener('click', e => {
     e.preventDefault()
     let incrementor
+
     if(e.target.matches('[data-next]')){
         incrementor = 1
     } else if(e.target.matches('[data-prev]')){
@@ -38,3 +50,30 @@ function showCurrentStep () {
         step.classList.toggle('active', index === currentStep)
     })
 }
+
+function showCurrentCountStep( ){
+    
+}
+
+let currentPlan = formPlans.findIndex(plan => {
+    return plan.classList.contains('active')
+})
+
+if(currentPlan < 0) {
+    currentPlan = 0
+    showCurrentPlan()
+}
+
+formPlans.forEach((plan, index) => {
+    plan.addEventListener('click', e => {
+        currentPlan = index
+        showCurrentPlan()
+    })
+})
+
+function showCurrentPlan () {
+    formPlans.forEach((plan, index) => {
+        plan.classList.toggle('active', index === currentPlan)
+    })
+}
+console.log(currentPlan)
