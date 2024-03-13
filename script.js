7de275e55a3e39f74810ecd7fe08938a55f71d0d
04stefke <jovanovics399@yahoo.com>
2024-03-13 10:20:12 +0000

    added the functionality for the navigation

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ let currentStep = formSteps.findIndex(step => {
 console.log(currentStep)
 if(currentStep < 0){
     currentStep = 0
-    formSteps[currentStep].classList.add('active')
+    showCurrentStep()
 }
-
 console.log(currentStep)
+
+multiStepForm.addEventListener('click', e => {
+    e.preventDefault()
+    if(e.target.matches('[data-next]')){
+        currentStep += 1
+    } else if(e.target.matches('[data-prev]')){
+        currentStep -= 1
+    }
+    showCurrentStep()
+})
+
+function showCurrentStep () {
+    formSteps.forEach((step, index) => {
+        step.classList.toggle('active', index === currentStep)
+    })
+}
