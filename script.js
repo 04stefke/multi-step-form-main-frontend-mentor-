883a56c24e96958888b0bac939f6d7ac15be8f81
04stefke <jovanovics399@yahoo.com>
2024-03-13 10:08:33 +0000

    making the step one default when starting

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const stepOneNumber = document.getElementById('stepOne')
-const stepTwoNumber = document.getElementById('stepTwo')
-const stepThreeNumber = document.getElementById('stepThree')
-const stepFourNumber = document.getElementById('stepFour')
+const multiStepForm = document.querySelector('[data-multi-step-form]')
+const formSteps = [...multiStepForm.querySelectorAll('[data-step]')]
+
+let currentStep = formSteps.findIndex(step => {
+    return step.classList.contains('active')
+})
+console.log(currentStep)
+if(currentStep < 0){
+    currentStep = 0
+    formSteps[currentStep].classList.add('active')
+}
+
+console.log(currentStep)
